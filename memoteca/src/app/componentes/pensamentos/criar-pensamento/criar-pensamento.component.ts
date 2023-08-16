cce06b5aa8b33a30f2a2f66e57f21c214731f7a0
Gustavo Tadei <gustavo.tadei@trt14.jus.br>
2023-08-16 13:41:15 -0400

    att

--- memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { minusculoValidator } from './minusculoValidator';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -27,22 +28,32 @@ export class CriarPensamentoComponent implements OnInit {
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
-      conteudo: ['Formulario reativo'],
-      autoria: ['Angular'],
-      modelo: ['modelo1']
+      conteudo: ['', Validators.compose([ Validators.required, Validators.pattern(/(.|\s)*\S(.|\s)*/) ])],
+      autoria: ['', Validators.compose([ Validators.required, Validators.minLength(3), minusculoValidator ])],
+      modelo: ['modelo1'],
+      favorito: [false]
     })
   }
 
   criarPensamento() {
-    this.service.criar(this.formulario.value).subscribe(() => {
-      this.router.navigate(['/listarPensamento'])
-    })
+    console.log(this.formulario.get('autoria')?.errors)
+
+    if(this.formulario.valid) {
+      this.service.criar(this.formulario.value).subscribe(() => {
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
     // alert("Novo pensamento criado")
   }
 
   cancelar() {
     this.router.navigate(['/listarPensamento'])
     // alert("Cancelado")
+  }
+
+  habilitarBotao(): string {
+    if(this.formulario.valid) return 'botao'
+    else return'botao__desabilitado'
   }
 
 }
