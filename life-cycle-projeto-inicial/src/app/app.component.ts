f283d3f7cc569bc9f3c4a02dd98a2a503e84c645
Gustavo Tadei <gustavo.tadei@trt14.jus.br>
2023-08-24 12:15:17 -0400

    att

---
+++ life-cycle-projeto-inicial/src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { Item } from './interfaces/iItem';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, DoCheck {
+  title = 'app-lista-de-compras';
+  listaDeCompra! : Array<Item>;
+  itemParaSerEditado! : Item;
+
+  constructor(private listaService : ListaDeCompraService) { }
+
+  ngOnInit(): void {
+    this.listaDeCompra = this.listaService.getListaDeCompra();
+    console.log(this.listaDeCompra);
+  }
+
+  ngDoCheck() {
+    console.log('DoCheck foi chamado');
+    this.listaService.atualizarLocalStorage();
+  }
+
+  editarItem(item: Item) {
+    this.itemParaSerEditado = item;
+  }
+
+}
